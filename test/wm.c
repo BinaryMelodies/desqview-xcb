43695937955db68d08d535ee354f37d361e0d35f
Binary Melodies <192211378+BinaryMelodies@users.noreply.github.com>
2025-10-24 12:59:23 +0200

    Added tests

---
+++ test/wm.c
@@ -0,0 +1,69 @@
+// https://www.x.org/releases/X11R7.7/doc/libxcb/tutorial/index.html
+
+#include <stdio.h>
+#include <string.h>
+
+#include <xcb/xcb.h>
+
+int
+main ()
+{
+  xcb_connection_t *c;
+  xcb_screen_t     *screen;
+  xcb_window_t      win;
+  char             *title = "Hello World !"
+#if __WATCOMC__
+    " (Watcom)"
+#elif __GNUC__
+    " (GNU)"
+#endif
+      ;
+  char             *title_icon = "Hello World !"
+#if __WATCOMC__
+    " (Watcom)"
+#elif __GNUC__
+    " (GNU)"
+#endif
+      " (iconified)";
+
+
+  /* Open the connection to the X server */
+  c = xcb_connect (NULL, NULL);
+
+  /* Get the first screen */
+  screen = xcb_setup_roots_iterator (xcb_get_setup (c)).data;
+
+  /* Ask for our window's Id */
+  win = xcb_generate_id (c);
+
+  /* Create the window */
+  xcb_create_window (c,                             /* Connection          */
+                     0,                             /* depth               */
+                     win,                           /* window Id           */
+                     screen->root,                  /* parent window       */
+                     0, 0,                          /* x, y                */
+                     250, 150,                      /* width, height       */
+                     10,                            /* border_width        */
+                     XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class               */
+                     screen->root_visual,           /* visual              */
+                     0, NULL);                      /* masks, not used     */
+
+  /* Set the title of the window */
+  xcb_change_property (c, XCB_PROP_MODE_REPLACE, win,
+                       XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8,
+                       strlen (title), title);
+
+  /* Set the title of the window icon */
+  xcb_change_property (c, XCB_PROP_MODE_REPLACE, win,
+                       XCB_ATOM_WM_ICON_NAME, XCB_ATOM_STRING, 8,
+                       strlen(title_icon), title_icon);
+
+  /* Map the window on the screen */
+  xcb_map_window (c, win);
+
+  xcb_flush (c);
+
+  getchar();
+
+  return 0;
+}
